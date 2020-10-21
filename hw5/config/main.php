0fbc13f75b16641180334726f4d5aed7b8d6f522
a3330 <a33303@mail.ru>
2020-10-21 23:17:23 +0300

    Home_work_5
    Успел сделать только часть. выбивает ошибку
    
    Warning: require_once(F:/OpenServer/domains/localhost/../config/main.php): failed to open stream: No such file or directory in F:\OpenServer\domains\localhost\public\index.php on line 2
    
    Fatal error: require_once(): Failed opening required 'F:/OpenServer/domains/localhost/../config/main.php' (include_path='.') in F:\OpenServer\domains\localhost\public\index.php on line 2

---
+++ hw5/config/main.php
@@ -0,0 +1,12 @@
+<?php
+define('ROOT_DIR', __DIR__ . '/../');
+define('PUBLIC_DIR', ROOT_DIR . 'public/');
+define('ENGINE_DIR', ROOT_DIR . 'engine/');
+define('IMG_DIR', PUBLIC_DIR . 'img/');
+define('VIEWS_DIR', ROOT_DIR . 'views/');
+
+$host = '127.0.0.1';
+$login = 'root';
+$password = 'root';
+$db = 'php2020';
+?>
